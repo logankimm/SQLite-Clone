c8df63e4505b62586649b482bc57c607c9a569bb
logankimm <logan.kim@utexas.edu>
2024-10-24 16:38:58 -0500

    adding explanation for block storage

--- DatabaseCore/Block.cs
+++ DatabaseCore/Block.cs
@@ -4,5 +4,18 @@ namespace DatabaseCore;
 
 public class Block : IBlock
 {
-    
+    readonly uint id;
+
+    public event EventHandler Disposed;
+
+    public uint Id {
+        get {
+            return id;
+        }
+    }
+
+    public Block(BlockStorage storage, uint Id, Stream stream)
+    {
+
+    }
 }
