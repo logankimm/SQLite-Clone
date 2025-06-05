629e6acbb256b6261a93dff5c1791995038eb073
logankimm <logan.kim@utexas.com>
2025-06-04 19:38:01 -0500

    Testing to see if DatabaseCore is integrated and works

--- DatabaseApplication/Program.cs
+++ DatabaseApplication/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using DatabaseCore;
 
 namespace DatabaseApplication;
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var asdf = new Test();
+        asdf.testing123();
     }
 }
