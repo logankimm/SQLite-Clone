6dda7a48f0fbef5652e69bcf0e8868f55d44efde
logankimm <logan.kim@utexas.com>
2025-08-04 04:03:59 -0500

    Initializing application files

---
+++ DatabaseApplication/ChampDatabase.cs
@@ -0,0 +1,6 @@
+// Type of data being stored within the database
+namespace DatabaseApplication;
+public class ChampDatabase
+{
+    readonly Stream mainDatabaseFile;
+}
--- DatabaseApplication/ChampModel.cs
+++
@@ -1,9 +0,0 @@
-// Type of data being stored within the database
-public class ChampModel
-{
-    public Guid Id { get; set; }
-    public string name { get; set; }
-    public int cost { get; set; }
-    public string setName { get; set; }
-    public byte[] unitData { get; set; }
-}
